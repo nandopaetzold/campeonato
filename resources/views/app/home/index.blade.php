dab73bb1aeab19aff3b84b220ea4c793b65ed54d
Fernando dos Santos Paetzold <fernando@paetservices.com>
2022-05-29 21:52:12 -0300

    Alterado algumas README

--- resources/views/app/home/index.blade.php
+++ resources/views/app/home/index.blade.php
@@ -12,11 +12,20 @@
                         </h4>
                     </div>
                     <div class="card-body">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Quos blanditiis tenetur unde suscipit, quam beatae rerum
-                            inventore consectetur, neque doloribus, cupiditate numquam
-                            dignissimos laborum odit minima. Eum quibusdam id?
+
+                        <h1>Meu campeonato</h1>
+
+                        <p>1- Criar o banco de dados</p>
+                        <p>2- Subir os migrate</p>
+                        <p>3- Adicionar os time e campeonatos</p>
+
+
+                        <h4>Como utilizar</h4>
+
+                        <p>Para acompanhar os jogos clique no botão vermelho referente ao campeonato.<br>
+                            Ao clicar no botão -> Gerar lista de jogos, é possivel informar os dados dos jogos da rodada ou
+                            criar uma rodada nova.<br>
+                            No botão Ranking é possivel ver o ranking dos times.<br>
                         </p>
                     </div>
                 </div>
--- resources/views/app/navbar.blade.php
+++ resources/views/app/navbar.blade.php
@@ -1,5 +1,6 @@
 <header class="container-fluid">
     <ul>
+        <li><a href="{{route('home')}}">Home</a></li>
         <li><a href="{{route('teams')}}">Gerir Times</a></li>
         <li><a href="{{route('championships')}}">Campeonatos</a></li>
     </ul>
